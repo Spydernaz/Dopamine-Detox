efff7531b445c2706e4e33a3beae054a89045f94
Nathaniel Vala <nathanielvala@hotmail.com>
2020-10-20 07:36:27 +1100

    updated the timezone info to stop negative hours and added post route to update the latest reset manually

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template, url_for, copy_current_request_context, redirect
+from flask import Flask, render_template, url_for, request, redirect
 from random import random
 from time import sleep
-from datetime import datetime
-from threading import Thread, Event
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 
-timer_date = (datetime.utcnow()).strftime("%d %b %Y, %H:%M:%S")
-
+timer_date = (datetime.utcnow() + timedelta(hours=11)).strftime("%d %b %Y, %H:%M:%S")
 
 @app.route('/')
 def index():
@@ -32,6 +30,18 @@ def reset_timer():
 def get_latestreset():
     global timer_date
     return timer_date
+
+@app.route("/api/settime", methods=["POST"])
+def post_settime():
+    global timer_date
+    timer_date = request.json["time"]
+
+    # datetime_object = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
+    # "20 Oct 2020, 06:18:12"
+    # datetime_object = datetime.strptime(request.json["time"], '%d %b %Y, %H:%M:%S')
+
+    return f"Set time to: {timer_date}"
+
 
 if __name__ == '__main__':
     app.run()
